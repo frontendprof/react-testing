2267388e4c044f52db68c28bfa3698029416330e
frontendprof <abdumaliksharipov@gmail.com>
2020-09-06 23:09:33 +0500

    Working on loading spinner

---
+++ src/Spinner.js
@@ -0,0 +1,16 @@
+
+
+import React from "react";
+
+
+const Spinner=()=>{
+
+    return(
+        <div className="ui active dimmer">
+            <div className="ui massive text loader">Loading ...</div>
+        </div>
+    )
+}
+
+
+export default Spinner;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 
 
@@ -37,7 +38,7 @@ class App extends React.Component{
 
     };
 
-    return <div>Loading ...</div>
+    return <Spinner />;
     
   }
 }

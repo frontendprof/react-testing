fea9e6732a2ac552ce055fd79bf0aaf8ac6489fd
frontendprof <abdumaliksharipov@gmail.com>
2020-09-06 21:43:59 +0500

    Determining season via conditional rendering

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -3,13 +3,29 @@ import React from 'react';
 
 
 
+const getSeason=(lat,month)=>{
+
+    if(month>2 && month<9){
+        return lat>0?"summer":"winter";
+    }else{
+        return lat>0?"winter":"summer";
+    }
+
+}
+
 
 
 const SeasonDisplay=(props)=> {
-    console.log(props);
+
+    const season=getSeason(props.lat,new Date().getMonth());
+
+    console.log(season);
+    
+
+
     return (
         <div>
-            "Hello"
+            Season Display
         </div>
     )
 }

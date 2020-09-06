69ef027d529c09e22b41a21ffe22ecfe016dbbdb
frontendprof <abdumaliksharipov@gmail.com>
2020-09-06 22:18:19 +0500

    Getting semantic icons to display

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -19,13 +19,17 @@ const SeasonDisplay=(props)=> {
 
     const season=getSeason(props.lat,new Date().getMonth());
 
-    const text=season==="winter"?"Burr, it is chilly":"Let's go for taraweh prayer";
+    const text=season==="winter"?"Burr, it is chilly":"Let's go swimming";
+    const icon=season==="winter"?"snowflake":"sun";
     
 
 
     return (
         <div>
+            <i className={`${icon} icon`}></i>
             <h1>{text}</h1>
+            <i className={`${icon} icon`}></i>
+
         </div>
     )
 }
